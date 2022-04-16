a2056b83f9c07c44824d671e42a6c220fb574ab8
webmaster-k0ara <webmaster.k0ala.jurimarikikiburu@gmail.com>
2022-04-16 15:33:10 +0900

    like counter

--- js/main.js
+++ js/main.js
@@ -1,49 +1,39 @@
 (function() {
   'use strict';
 
+
+  var likeComponent = Vue.extend({
+    props: {
+      message: {
+        type: String,
+        default: 'Like'
+      }
+    },
+    data: function(){
+      return {
+        count: 0,
+      }
+    },
+    template: '<button @click="countUp">{{ message }}{{ count }}</button>',
+    methods: {
+      countUp: function (){
+        this.count++;
+        this.$emit('increment');
+      }
+    }
+  });
+
   var vm = new Vue({
     el: '#app',
+    components: {
+      'like-component': likeComponent
+    },
     data: {
-      newItem: '',
-      todos: []
-    },
-    watch: {
-      todos: {
-        handler: function() {
-          localStorage.setItem('todos', JSON.stringify(this.todos));
-        },
-        deep: true
-      }
-    },
-    mounted: function() {
-      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+      total: 0
     },
     methods: {
-      addItem: function() {
-        var item = {
-          title: this.newItem,
-          isDone: false
-        };
-        this.todos.push(item);
-        this.newItem = '';
-      },
-      deleteItem: function(index) {
-        if (confirm('are you sure?')) {
-          this.todos.splice(index, 1);
-        }
-      },
-      purge: function() {
-        if (!confirm('delete finished?')) {
-          return;
-        }
-        this.todos = this.remaining;
-      }
-    },
-    computed: {
-      remaining: function() {
-        return this.todos.filter(function(todo) {
-          return !todo.isDone;
-        });
+      incrementTotal: function(){
+        this.total++;
       }
     }
   });
